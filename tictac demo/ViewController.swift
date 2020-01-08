1fac114204bab2a49789fb3198e37390a08c04cb
Sumandeep2111 <sumandeep21111994@gmail.com>
2020-01-07 19:46:03 -0500

    game is working

--- tictac demo/ViewController.swift
+++ tictac demo/ViewController.swift
@@ -9,12 +9,47 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var activePlayer = 1
+    var activeGame = true
+    var gameState = [0,0,0,0,0,0,0,0,0]
+    let winningcombinations = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func btnpressed(_ sender: UIButton) {
+        print(sender.tag)
+        let activePosition = sender.tag - 1
+        if gameState[activePosition] == 0 && activeGame {
+            gameState[activePosition] = activePlayer
+            if activePlayer == 1 {
+                sender.setImage(UIImage(named: "nought.png"), for: [])
+                       activePlayer = 2
+                   }else {
+                       sender.setImage(UIImage(named: "cross.png"), for: [])
+                       activePlayer = 1
+                   }
+        }
+        
+        for combination in winningcombinations {
+            if gameState[combination[0]] != 0 && gameState[combination[1]] == gameState[combination[2]] && gameState[combination[2]] == gameState[combination[0]] {
+                
+                activeGame = false
+                
+                if gameState[combination[0]] == 1{
+                    print("winner is nought")
+                }
+                else {
+                    print("winner is cross")
+                }
+                
+            }
+        }
+       
+    }
+    
 }
 
